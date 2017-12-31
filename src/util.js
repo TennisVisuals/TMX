4d3643e0d174def9ba2269c96ce3441fd749da4a
TennisVisuals <tennis.aip@gmail.com>
2017-12-31 11:49:21 +0100

    more robust list membership with better grammar

--- src/util.js
+++ src/util.js
@@ -170,6 +170,7 @@ let util = function() {
    }
 
    // Miscellaneous Functions
+   fx.isMember = (list, m) => list.reduce((p, c) => c == m || p, false);
    fx.unique = (arr) => arr.filter((item, i, s) => s.lastIndexOf(item) == i);
    fx.subSort = (arr, i, n, sortFx) => [].concat(...arr.slice(0, i), ...arr.slice(i, i + n).sort(sortFx), ...arr.slice(i + n, arr.length));
    fx.inPlaceSubSort = (arr, i, n, sortFx) => {
