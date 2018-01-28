0dca3effafd881401b237972e5b6fa3e219b94c8
TennisVisuals <tennis.aip@gmail.com>
2018-01-28 18:15:29 +0100

    version bump(s)

--- src/config.js
+++ src/config.js
@@ -41,7 +41,7 @@ let config = function() {
    // END queryString
 
    var env = {
-      version: '0.9.49',
+      version: '0.9.55',
       version_check: undefined,
       org: {
          name: undefined,
