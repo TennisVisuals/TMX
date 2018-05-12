04b506d55c397321c7f6484893081d713e93f292
TennisVisuals <tennis.aip@gmail.com>
2018-05-12 08:16:04 +0200

    dom safe uuid.idGen()

--- src/modules/UUID.js
+++ src/modules/UUID.js
@@ -39,6 +39,7 @@ export const UUID = function() {
 
   self.new = () => formatUuid(getRandomValuesFunc());
 
+  self.idGen = () => `_${self.generate()}`;
   self.generate = function() {
     var d0 = Math.random()*0xffffffff|0;
     var d1 = Math.random()*0xffffffff|0;
