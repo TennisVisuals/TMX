37f64da3a26a53c7f9c50e3dc687c042a1b88559
TennisVisuals <tennis.aip@gmail.com>
2018-06-28 07:18:55 +0200

    rename internally from fx. to util. & add date formats

--- src/modules/util.js
+++ src/modules/util.js
@@ -2,11 +2,11 @@ import { Diacritics } from './Diacritics';
 import { cleanScore } from './cleanScore';
 
 export const util = function() {
-   let fx = {};
+   let util = {};
 
-   fx.clearHistory = () => { history.pushState('', document.title, window.location.pathname); }
+   util.clearHistory = () => { history.pushState('', document.title, window.location.pathname); }
 
-   fx.HHMMSS = (s, format) => {
+   util.HHMMSS = (s, format) => {
       var sec_num = parseInt(s, 10); // don't forget the second param
       var hours   = Math.floor(sec_num / 3600);
       var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
@@ -20,28 +20,28 @@ export const util = function() {
       return display_seconds ? hours+':'+minutes+':'+seconds : hours+':'+minutes;
    }
 
-   fx.replaceDiacritics = (text) => Diacritics.replace(text);
-   fx.nameHash = (name) => fx.replaceDiacritics(name.replace(/[-_,.\ ]+/g, "")).toLowerCase();
+   util.replaceDiacritics = (text) => Diacritics.replace(text);
+   util.nameHash = (name) => util.replaceDiacritics(name.replace(/[-_,.\ ]+/g, "")).toLowerCase();
 
-   fx.parseInt = (value) => {
+   util.parseInt = (value) => {
       let result = parseInt(value);
       return isNaN(result) ? undefined : result;
    }
 
-   fx.normalizeScore = (score) => {
+   util.normalizeScore = (score) => {
       let clean_score = cleanScore.normalize(score);
       if (clean_score) return clean_score.join(' ');
       alert(`Score can't be normalized: ${score}`);
       return score;
    }
 
-   fx.string2boolean = (string) => {
+   util.string2boolean = (string) => {
       if (typeof string == 'boolean') return string;
       if (string === 'true') return true;
       if (string === 'false') return false;
    }
 
-   fx.weekDays = (date) => {
+   util.weekDays = (date) => {
       let dates = [0, 1, 2, 3, 4, 5, 6].map(i => dayOfWeek(date, i));
       return dates;
 
@@ -54,21 +54,27 @@ export const util = function() {
             
    }
 
-   fx.formatDate = (date, separator = '-') => {
+   util.formatDate = formatDate;
+   function formatDate(date, separator = '-', format='YMD') {
       if (!date) return '';
 
-      let d = new Date(date),
-         month = '' + (d.getMonth() + 1),
-         day = '' + d.getDate(),
-         year = d.getFullYear();
+      let d = new Date(date);
+      let month = '' + (d.getMonth() + 1);
+      let day = '' + d.getDate();
+      let year = d.getFullYear();
 
       if (month.length < 2) month = '0' + month;
       if (day.length < 2) day = '0' + day;
 
+      if (format == 'DMY') return [day, month, year].join(separator);
+      if (format == 'MDY') return [month, day, year].join(separator);
+      if (format == 'YDM') return [year, day, month].join(separator);
+      if (format == 'DYM') return [day, year, month].join(separator);
+      if (format == 'MYD') return [month, year, day].join(separator);
       return [year, month, day].join(separator);
    }
 
-   fx.performTask = (fx, data, bulkResults = true) => {
+   util.performTask = (fx, data, bulkResults = true) => {
       return new Promise(function(resolve, reject) {
          let results = [];
          if (!data.length) return reject();
@@ -92,10 +98,10 @@ export const util = function() {
       });
    }
 
-   fx.normalizeName = (name, noaccents = true) => {
+   util.normalizeName = (name, noaccents = true) => {
       if (!name) { return ''; }
       var particles = ['del', 'de', 'di', 'du', 'van', 'von', 'ten']; 
-      if (noaccents) name = fx.replaceDiacritics(name.trim());
+      if (noaccents) name = util.replaceDiacritics(name.trim());
       name = name.replace(/\s+/g,' ').trim();
       var nNames = name.split(' ').map(m => m.toLowerCase());
 
@@ -121,7 +127,7 @@ export const util = function() {
       function replaceAt(s, n, t) { return s.substring(0, n) + t + s.substring(n + 1); }
    }
 
-   fx.keyWalk = keyWalk;
+   util.keyWalk = keyWalk;
    function keyWalk(valuesObject, optionsObject) {
       if (!valuesObject || !optionsObject) return;
       var vKeys = Object.keys(valuesObject);
@@ -139,13 +145,13 @@ export const util = function() {
       }
    }
 
-   fx.boolAttrs = boolAttrs;
+   util.boolAttrs = boolAttrs;
    function boolAttrs(optionsObject) {
       if (!optionsObject) return;
       var oKeys = Object.keys(optionsObject);
       for (var k=0; k < oKeys.length; k++) {
         var oo = optionsObject[oKeys[k]];
-        if (typeof oo == 'object' && typeof vo !== 'function' && oo.constructor !== Array) {
+        if (typeof oo == 'object' && oo.constructor !== Array) {
             boolAttrs(optionsObject[oKeys[k]]);
         } else {
             if (oo === 'true' || oo == true) {
@@ -159,17 +165,34 @@ export const util = function() {
       }
    }
 
+   util.fxAttrs = fxAttrs;
+   function fxAttrs(optionsObject) {
+      if (!optionsObject) return;
+      var oKeys = Object.keys(optionsObject);
+      for (var k=0; k < oKeys.length; k++) {
+        var oo = optionsObject[oKeys[k]];
+        if (typeof oo == 'object') {
+            fxAttrs(optionsObject[oKeys[k]]);
+        } else {
+            if (oKeys[k] == 'fx' && typeof oo == 'string') {
+               try { optionsObject.fx = createFx(optionsObject.fx); }
+               catch (err) { util.logError(err); }
+            }
+        }
+      }
+   }
+
    // DOM
-   fx.getParent = (elem, class_name) => Array.from(elem.classList).indexOf(class_name) >= 0 ? elem : findUpClass(elem, class_name);
+   util.getParent = (elem, class_name) => Array.from(elem.classList).indexOf(class_name) >= 0 ? elem : findUpClass(elem, class_name);
 
    /*
-   fx.selectParent = (elem, class_name) => {
-      let container = fx.getParent(elem, class_name);
+   util.selectParent = (elem, class_name) => {
+      let container = util.getParent(elem, class_name);
       return d3.select(container);
    }
    */
 
-   fx.findUpClass = findUpClass;
+   util.findUpClass = findUpClass;
    function findUpClass(el, class_name) {
       while (el.parentNode) {
          el = el.parentNode;
@@ -178,7 +201,7 @@ export const util = function() {
       return null;
    }
 
-   fx.getChildrenByClassName = getChildrenByClassName;
+   util.getChildrenByClassName = getChildrenByClassName;
    function getChildrenByClassName(elem, className) {
      var matches  = [];
      function traverse(node) {
@@ -191,32 +214,32 @@ export const util = function() {
      return matches;
    }
 
-   fx.addEventToClass = (cls, fx, node = document, e = 'click') => {
+   util.addEventToClass = (cls, fx, node = document, e = 'click') => {
       Array.from(node.querySelectorAll('.' + cls)).forEach(elem => elem.addEventListener(e, fx)); 
    }
 
    // Miscellaneous Functions
-   fx.zeroPad = (number) => number.toString()[1] ? number : "0" + number;
-   fx.numeric = (value) => value && !isNaN(value) ? parseInt(value.toString().trim()) : 0;
-   fx.containsNumber = (value) => /\d/.test(value);
-   fx.isMember = (list, m) => list.reduce((p, c) => c == m || p, false);
-   fx.unique = (arr) => arr.filter((item, i, s) => s.lastIndexOf(item) == i);
-   fx.uunique = (arr) => Object.keys(Object.assign({}, ...arr.map(a=>({[a]:true}))));
-   fx.subSort = (arr, i, n, sortFx) => [].concat(...arr.slice(0, i), ...arr.slice(i, i + n).sort(sortFx), ...arr.slice(i + n, arr.length));
-   fx.inPlaceSubSort = (arr, i, n, sortFx) => {
+   util.zeroPad = (number) => number.toString()[1] ? number : "0" + number;
+   util.numeric = (value) => value && !isNaN(value) ? parseInt(value.toString().trim()) : 0;
+   util.containsNumber = (value) => /\d/.test(value);
+   util.isMember = (list, m) => list.reduce((p, c) => c == m || p, false);
+   util.unique = (arr) => arr.filter((item, i, s) => s.lastIndexOf(item) == i);
+   util.uunique = (arr) => Object.keys(Object.assign({}, ...arr.map(a=>({[a]:true}))));
+   util.subSort = (arr, i, n, sortFx) => [].concat(...arr.slice(0, i), ...arr.slice(i, i + n).sort(sortFx), ...arr.slice(i + n, arr.length));
+   util.inPlaceSubSort = (arr, i, n, sortFx) => {
       let newarray = [].concat(...arr.slice(0, i), ...arr.slice(i, i + n).sort(sortFx), ...arr.slice(i + n, arr.length));
       arr.length = 0;
       arr.push.apply(arr, newarray);
       return arr;
    }
-   fx.arrayMap = (array) => new Map([...new Set(array)].map( x => [x, array.filter(y => y === x).length]));
-   fx.arrayCount = (array) => array.reduce((r,k) => { r[k] = ++r[k] || 1; return r},{});
-   fx.occurrences = (val, arr) => arr.reduce((r,val) => { r[val] = 1+r[val] || 1; return r},{})[val] || 0;
-   fx.intanceCount = (values) => values.reduce((a, c)=>{a[c]++?0:a[c]=1;return a},{});
-   fx.indices = (val, arr) => arr.reduce((a, e, i) => { if (e === val) a.push(i); return a; }, []) 
-   fx.randomPop = (array) => array.length ? array.splice(Math.floor(Math.random()*array.length), 1)[0] : undefined;
+   util.arrayMap = (array) => new Map([...new Set(array)].map( x => [x, array.filter(y => y === x).length]));
+   util.arrayCount = (array) => array.reduce((r,k) => { r[k] = ++r[k] || 1; return r},{});
+   util.occurrences = (val, arr) => arr.reduce((r,val) => { r[val] = 1+r[val] || 1; return r},{})[val] || 0;
+   util.intanceCount = (values) => values.reduce((a, c)=>{a[c]++?0:a[c]=1;return a},{});
+   util.indices = (val, arr) => arr.reduce((a, e, i) => { if (e === val) a.push(i); return a; }, []) 
+   util.randomPop = (array) => array.length ? array.splice(Math.floor(Math.random()*array.length), 1)[0] : undefined;
 
-   fx.hashReduce = (arr) => {
+   util.hashReduce = (arr) => {
       return arr.reduce((l, c) => {
          let hash = c.sort().join('|');
          if (l.indexOf(hash) < 0) l.push(hash);
@@ -224,19 +247,19 @@ export const util = function() {
       }, []).map(r=>r.split('|'));
    }
 
-   // fx.missingNumbers = (a) => Array.from(Array(Math.max(...a)).keys()).map((n, i) => a.indexOf(i) < 0? i : null).filter(f=>f);
-   fx.missingNumbers = (a, l=true) => Array.from(Array(Math.max(...a)).keys()).map((n, i) => a.indexOf(i) < 0  && (!l || i > Math.min(...a)) ? i : null).filter(f=>f);
-   fx.range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
-   fx.numArr = (count) => [...Array(count)].map((_, i) => i);
-   fx.intersection = (a, b) => a.filter(n => b.indexOf(n) !== -1).filter((e, i, c) => c.indexOf(e) === i);
+   // util.missingNumbers = (a) => Array.from(Array(Math.max(...a)).keys()).map((n, i) => a.indexOf(i) < 0? i : null).filter(f=>f);
+   util.missingNumbers = (a, l=true) => Array.from(Array(Math.max(...a)).keys()).map((n, i) => a.indexOf(i) < 0  && (!l || i > Math.min(...a)) ? i : null).filter(f=>f);
+   util.range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
+   util.numArr = (count) => [...Array(count)].map((_, i) => i);
+   util.intersection = (a, b) => a.filter(n => b.indexOf(n) !== -1).filter((e, i, c) => c.indexOf(e) === i);
 
-   fx.passFail = (array, conditionFx) => {
+   util.passFail = (array, conditionFx) => {
       let result = { pass: [], fail: [] };
       array.forEach(item => result[conditionFx(item) ? 'pass' : 'fail'].push(item));
       return result;
    }
 
-   fx.swapElements = (obj1, obj2) => {
+   util.swapElements = (obj1, obj2) => {
       // save the location of obj2
       var parent2 = obj2.parentNode;
       var next2 = obj2.nextSibling;
@@ -258,19 +281,19 @@ export const util = function() {
          }
       }
    }
-   fx.moveNode = (destination_id, source_id) => {
+   util.moveNode = (destination_id, source_id) => {
       let source = document.getElementById(source_id);
       let target = source.parentNode.removeChild(source);
       let destination = document.getElementById(destination_id);
       destination.innerHTML = '';
       destination.append(target);
    }
-   fx.log2 = (val) => Math.log(val) / Math.LN2;
-   fx.nearestPow2 = (val) => Math.pow(2, Math.round( Math.log(val) / Math.log(2)));
+   util.log2 = (val) => Math.log(val) / Math.LN2;
+   util.nearestPow2 = (val) => Math.pow(2, Math.round( Math.log(val) / Math.log(2)));
 
-   fx.validDate = (datestring, range) => {
+   util.validDate = (datestring, range) => {
       if (!datestring) return false;
-      let dateparts = fx.formatDate(datestring).split('-');
+      let dateparts = formatDate(datestring).split('-');
       if (isNaN(dateparts.join(''))) return false;
       if (dateparts.length != 3) return false;
       if (dateparts[0].length != 4) return false;
@@ -281,7 +304,7 @@ export const util = function() {
       return true;
    }
 
-   fx.isDate = (dateArg) => {
+   util.isDate = (dateArg) => {
       if (typeof dateArg == 'boolean') return false;
       var t = (dateArg instanceof Date) ? dateArg : !isNaN(dateArg) ? new Date(dateArg) : false;
       return t && !isNaN(t.valueOf());
@@ -291,10 +314,10 @@ export const util = function() {
       return (new Date(minDate) <= new Date(maxDate));
    }
 
-   fx.dateUTC = (date) => Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
+   util.dateUTC = (date) => Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
 
-   fx.dateRange = (startDt, endDt) => {
-       var error = ((fx.isDate(endDt)) && (fx.isDate(startDt)) && isValidDateRange(startDt, endDt)) ? false : true;
+   util.dateRange = (startDt, endDt) => {
+       var error = ((util.isDate(endDt)) && (util.isDate(startDt)) && isValidDateRange(startDt, endDt)) ? false : true;
        var between = [];
        if (error) {
           console.log('error occured!!!... Please Enter Valid Dates');
@@ -309,16 +332,16 @@ export const util = function() {
        return between;
    }
 
-   fx.sameDay = (d1, d2) => {
+   util.sameDay = (d1, d2) => {
      return d1.getFullYear() === d2.getFullYear() &&
        d1.getMonth() === d2.getMonth() &&
        d1.getDate() === d2.getDate();
    }
 
-   fx.isOverflowing = (el) => { return el.scrollWidth > el.clientWidth; }
-   fx.scaleFont = (el) => {
+   util.isOverflowing = (el) => { return el.scrollWidth > el.clientWidth; }
+   util.scaleFont = (el) => {
       let counter = 0;
-      while (fx.isOverflowing(el) && counter < 20) {
+      while (util.isOverflowing(el) && counter < 20) {
          let font_size = el.style.fontSize;
          let size = font_size.match(/[\.\d]+/)[0];
          let units = font_size.match(/[A-Za-z]+/)[0];
@@ -326,7 +349,8 @@ export const util = function() {
       }
    }
 
-   fx.createFx = (str) => {
+   util.createFx = createFx;
+   function createFx(str) {
       var startBody = str.indexOf('{') + 1;
       var endBody = str.lastIndexOf('}');
       var startArgs = str.indexOf('(') + 1;
@@ -334,7 +358,7 @@ export const util = function() {
       return new Function(str.substring(startArgs, endArgs), str.substring(startBody, endBody));
    }
 
-   fx.logError = (err) => console.log(err);
+   util.logError = (err) => console.log(err);
 
-   return fx;
+   return util;
 }();
