fa0772c5c6cf4f004966db3176d0cc291fb6f0e6
TennisVisuals <tennis.aip@gmail.com>
2018-08-15 20:20:57 +0200

    cleanup; more robust class removal for situations with multiple simultaneous jsTabs instances

--- src/modules/jsTabs.js
+++ src/modules/jsTabs.js
@@ -14,28 +14,19 @@ export const jsTabs = function() {
       scrollIntoView: false,
    }
 
-   /*
-   function indexOfElem(elem){
-       let  i = 0;
-       let desiredNodeType = elem.nodeType;
-       while((elem=elem.previousSibling)!=null) {if(elem.nodeType===desiredNodeType)i++;}
-       return i;
-   }
-   */
-
-   let selectTab = (root, no, callback) => {
+   let selectTab = ({ root, no, callback, baseclass }) => {
       //update the tab's link
-      let e = root.querySelector(".jstabs .tabs > span.selected");
-      if (e) e.classList.remove("selected");
-      e = root.querySelector(".jstabs .tabs > span:nth-of-type(" + (no - 1) + ")")
-      if (e) e.classList.add("selected");
+      let ss = Array.from(root.querySelectorAll(`.${baseclass} .tabs > span.selected`));
+      ss.forEach(s=>s.classList.remove("selected"));
+      let s = root.querySelector(`.${baseclass} .tabs > span:nth-of-type(${no - 1})`)
+      if (s) s.classList.add("selected");
 
       //update the tab (div)
-      e = root.querySelector(".jstabs .tab.selected");
-      if (e) e.classList.remove("selected");
-      e = root.querySelector(".jstabs .tab:nth-of-type(" + no + ")");
-      if (e) e.classList.add("selected");
-      let reference = e ? e.getAttribute('reference') : '';
+      let st = Array.from(root.querySelectorAll(`.${baseclass} .tab.selected`));
+      st.forEach(s=>s.classList.remove("selected"));
+      let t = root.querySelector(`.${baseclass} .tab:nth-of-type(${no})`);
+      if (t) t.classList.add("selected");
+      let reference = t ? t.getAttribute('reference') : '';
 
       // untested
       if (o.scrollIntoView && document.body.scrollIntoView) document.body.scrollIntoView();
@@ -43,16 +34,16 @@ export const jsTabs = function() {
       if (callback && typeof callback == 'function') callback(no - 2, reference);
    }
 
-   jsTabs.load = (el, callback) => {
+   jsTabs.load = (el, callback, baseclass='jstabs') => {
       let root = el || document;
-      let es = root.querySelectorAll(".jstabs .tabs > span");
+      let es = root.querySelectorAll(`.${baseclass} .tabs > span`);
       for (let i = 0; i < es.length; i++) {
           (function (el,idx) {
-              let st = () => selectTab(root, idx + 1, callback);
+              let st = () => selectTab({ root, no: idx + 1, baseclass, callback });
               el.addEventListener('click', st);
           })(es[i],i+1);
       }
-      let displayTab = (tab=0) => selectTab(root, tab + 2);
+      let displayTab = (tab=0) => selectTab({ root, no: tab + 2, baseclass });
 
       // by default display the first tab
       displayTab();
@@ -60,8 +51,8 @@ export const jsTabs = function() {
       return displayTab;
    } 
 
-   jsTabs.generate = (tabs) => {
-      let html = `<div class="jstabs"><div class='hscroll'><div class="tabs">`;
+   jsTabs.generate = ({ tabs, shadow=true, baseclass='jstabs' }) => {
+      let html = `<div class="${baseclass} ${shadow ? 'shadow' : ''}"><div class='hscroll'><div class="tabs">`;
       html += tabs.map(o => {
          let id = o.id ? ` id="${o.id}"` : '';
          let display = o.display || 'inline';
