7dd9db1e6a2b95336888a0ab88be408bf0c6e584
TennisVisuals <tennis.aip@gmail.com>
2018-08-23 11:23:44 +0200

    cleanup

--- src/main.js
+++ src/main.js
@@ -1,14 +1,3 @@
-import { db } from './modules/db';
-import { UUID } from './modules/UUID';
-import { util } from './modules/util';
-import { theme } from './modules/theme';
-import { exportFx } from './modules/exportFx';
-import { scheduleFx } from './modules/scheduleFx';
-import { searchBox } from './modules/searchBox';
-import { matchObject } from './modules/matchObject';
-import { eventManager } from './modules/eventManager';
-import { tournamentDisplay } from './modules/tournamentDisplay';
-
 import { config } from './modules/config';
 import { playerFx } from './modules/playerFx';
 
@@ -27,43 +16,7 @@ if (window.attachEvent) {
    }
 }
 
-var elements = {
-   header:           document.getElementById('header'),
-   header_actions:   document.getElementById('header_actions'),
-   main:             document.getElementById('main'),
-   home:             document.getElementById('go_home'),
-   content:          document.getElementById('content'),
-   refresh:          document.getElementById('refresh'),
-   idiomatic:        document.getElementById('idiomatic'),
-   searchmeta:       document.getElementById('searchmeta'),
-   searchentry:      document.getElementById('searchentry'),
-   searchinput:      document.getElementById('searchinput'),
-   searchextra:      document.getElementById('searchextra'),
-   searchcount:      document.getElementById('searchcount'),
-   search_select:    document.getElementById('search_select'),
-   searchcategory:   document.getElementById('searchcategory'),
-}
-
 function start() {
-   db.addDev({db});
-   db.addDev({util});
-   db.addDev({UUID});
-   db.addDev({theme});
-   db.addDev({config});
-   db.addDev({playerFx});
-   db.addDev({exportFx});
-   db.addDev({searchBox});
-   db.addDev({scheduleFx});
-   db.addDev({matchObject});
-   db.addDev({eventManager});
-
    playerFx.displayTournament = tournamentDisplay.displayTournament;
-
    config.init();
 }
-
-/* TODO:
- *
- * 1. config.env() data must be added to tournament record when sent to mobilref
- *
- */
