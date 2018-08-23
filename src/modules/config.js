0037de07ebadc9bf1f3cedf0bc8fa85f04c5cd16
TennisVisuals <tennis.aip@gmail.com>
2018-08-23 11:13:19 +0200

    version bump

--- src/modules/config.js
+++ src/modules/config.js
@@ -60,7 +60,7 @@ export const config = function() {
 
    var env = {
       // version is Major.minor.added.changed.fixed
-      version: '1.0.39.53.35.m',
+      version: '1.0.40.56.37',
       version_check: undefined,
       reset_new_versions: false,
 
