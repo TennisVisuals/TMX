27c7eb2cbe220a13c4622f56b0ead291dad72195
TennisVisuals <tennis.aip@gmail.com>
2018-10-30 05:20:28 +0100

    new charts added

---
+++ src/modules/barChart.js
@@ -0,0 +1,101 @@
+export function barChart() {
+    let width;
+    let height;
+    let margin = {top: 10, right: 10, bottom: 10, left: 10};
+    let colour = d3.scaleOrdinal(d3.schemeCategory20c); // colour scheme
+
+    function chart(selection) {
+        selection.each(function(data) {
+
+           // initialize chart components
+           let x = d3.scaleBand()
+              .range([0, width])
+              .padding(0.1);
+           let y = d3.scaleLinear()
+              .range([height, 0]);
+
+           let y_max = d3.max(data, d=>d.value);
+           x.domain(d3.range(0, 101));
+           y.domain([0, y_max]);
+
+           let xticks = x.domain().filter((d,i) => !(i%10));
+           let xAxis = d3.axisBottom(x)
+              .tickValues(xticks);
+
+           let yticks = d3.range(0, y_max + 1);
+           let spacing = Math.ceil(yticks.length / 5);
+           yticks = yticks.filter(f=>f/spacing == Math.round(f/spacing));
+           let yAxis = d3.axisLeft(y)
+              .tickValues(yticks)
+              .tickFormat(d3.format(".0r"));
+
+           // ===========================================================================================
+           // append the svg object to the selection
+           let svg = selection.append("svg")
+               .attr("class", "drop-shadow")
+               .attr("width", width + margin.left + margin.right)
+               .attr("height", height + margin.top + margin.bottom)
+             .append("g")
+               .attr("transform", 
+                     "translate(" + margin.left + "," + margin.top + ")");
+           // ===========================================================================================
+
+           // generate chart
+           data.forEach(function(d) {
+              d.pct = +d.pct;
+              d.value = +d.value;
+           });
+
+           svg.selectAll(".bar")
+              .data(data)
+            .enter().append("rect")
+              .attr("class", "bar")
+              .attr("x", d => x(d.pct))
+              .attr("width", x.bandwidth())
+              .attr("y", d => y(d.value))
+              .attr("height", d => height - y(d.value))
+              .style("fill", colour)
+
+           svg.append("g")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+           svg.append("g")
+              .call(yAxis);
+
+        });
+    }
+
+    // getter and setter functions. See Mike Bostocks post "Towards Reusable Charts" for a tutorial on how this works.
+    chart.width = function(value) {
+        if (!arguments.length) return width;
+        width = value;
+        return chart;
+    };
+
+    chart.height = function(value) {
+        if (!arguments.length) return height;
+        height = value;
+        return chart;
+    };
+
+    chart.margin = function(value) {
+        if (!arguments.length) return margin;
+        margin = value;
+        return chart;
+    };
+
+    chart.radius = function(value) {
+        if (!arguments.length) return radius;
+        radius = value;
+        return chart;
+    };
+
+    chart.colour = function(value) {
+        if (!arguments.length) return colour;
+        colour = value;
+        return chart;
+    };
+
+    return chart;
+}
--- src/modules/tmxStats.js
+++ src/modules/tmxStats.js
@@ -3,17 +3,101 @@ import { util } from './util';
 import { drawFx } from './drawFx';
 import { lang } from './translator';
 import { matchFx } from './matchFx';
+import { barChart } from './barChart';
+import { donutChart } from './donutChart';
+import { scoreBoard } from './scoreBoard';
 
 export const tmxStats = function() {
 
    let fx = {};
 
-   fx.processMatches = (tournament) => {
+   let bands = { decisive: 20, routine: 50 };
+   let colours = { decisive: "#F48F06", routine: "#0369C3", competitive: "#2BC303", walkover: "#F9E8E5" }
+   let categories = { 'd': 'Decisive', 'c': 'Competitive', 'r': 'Routine', 'w': 'Walkover' }
+   let add = (a, b) => (a || 0) + (b || 0);
+
+   fx.processMatches = (tournament, container) => {
       let { completed_matches, pending_matches, upcoming_matches, total_matches } = matchFx.tournamentEventMatches({ tournament });
-      console.log('matches:', completed_matches);
+      let match_sets = completed_matches.map(match => {
+         let sets = scoreBoard.convertStringScore({ string_score: match.score, score_format: match.score_format || {}, winner_index: match.winner_index });
+         let games = sets.reduce((p, c) => { p[0] += c[0].games || 0; p[1] += c[1].games || 0; return p; }, [0, 0]);
+         let stb = sets.reduce((p, c) => { p[0] += c[0].supertiebreak || 0; p[1] += c[1].supertiebreak || 0; return p; }, [0, 0]);
+         if (stb.reduce(add)) { games[(stb[0] > stb[1]) ? 0 : 1] += 1; }
+         return { sets, games, score: match.score };
+      });
+
+      let buckets = pctSpread(match_sets).reduce((b, a) => { if (b[a]) { b[a] += 1 } else { b[a] = 1}; return b; }, {});
+      let data = Object.keys(buckets).map(key => isNaN(key) ? undefined : { pct: key, value: buckets[key] }).filter(f=>f);
+
+      let sortem = (p, c) => {
+         let band = isNaN(c) ? 'w' : c <= bands.decisive ? 'd' : c <= bands.routine ? 'r' : 'c';
+         p[band] += 1;
+         return p;
+      }
+      let dd = pctSpread(match_sets).reduce((p, c) => sortem(p, c), { w: 0, d: 0, r: 0, c: 0 }); 
+      let total = Object.keys(dd).reduce((a, k) => (dd[k] || 0) + a, 0);
+      let dt = Object.keys(dd).map(k => ({ Percentage: dd[k] / total, Category: categories[k] })); 
+
+      setTimeout(function() {
+         fx.pctDonut(container, dt, true);
+         fx.competitiveMatchIndices(container, data);
+      }, 400);
+   }
+
+   function gamesPct(match_results) {
+      let loser_games = Math.min(...match_results.games);
+      let winner_games = Math.max(...match_results.games);
+      let total_games = match_results.games.reduce(add);
+      let pct = Math.round((loser_games/winner_games) * 100);
+      return pct;
+   }
+   function pctSpread(pcts) { return pcts.map(gamesPct).sort().map(p=>p.toFixed(2)); }
+
+   fx.pctDonut = (container, data, clean) => {
+      let chart_div = container.stat_charts.element;
+      let root = d3.select(chart_div);
+
+      if (clean) root.selectAll("svg").remove();
+
+      let computed_style = window.getComputedStyle(chart_div);
+      let width_value = computed_style.width.match(/\d+/);
+      let width = (width_value && width_value.length && width_value[0] * .9) || (window.innerWidth - margin.left - margin.right);
+
+      let donut = donutChart()
+         .width(width)
+         .height(300)
+         .cornerRadius(3) // sets how rounded the corners are on each slice
+         .padAngle(0.015) // effectively dictates the gap between slices
+         .colour(d3.scaleOrdinal([colours.walkover, colours.decisive, colours.routine, colours.competitive]))
+         .variable('Percentage')
+         .category('Category');
+
+      root.datum(data).call(donut);
+   }
+
+   fx.competitiveMatchIndices = (container, data, clean) => {
+
+      var chart_div = container.stat_charts.element;
+      let root = d3.select(chart_div);
+
+      var margin = {top: 20, right: 20, bottom: 20, left: 40};
+      let computed_style = window.getComputedStyle(chart_div);
+      let width_value = computed_style.width.match(/\d+/);
+      let width = (width_value && width_value.length && width_value[0] * .9) || (window.innerWidth - margin.left - margin.right);
+
+      if (clean) root.selectAll("svg").remove();
+
+      let bar = barChart()
+         .width(width)
+         .height(200)
+         .margin(margin)
+         .colour(barColor)
+
+      root.datum(data).call(bar);
+
+      function barColor(d) { return (d.pct <= bands.decisive) ? colours.decisive : (d.pct <= bands.routine) ? colours.routine : colours.competitive; }
    }
 
    return fx;
 
 }();
-
