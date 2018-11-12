acd77da2727e80e18c0fce40854610fa15c6d783
TennisVisuals <tennis.aip@gmail.com>
2018-11-12 08:49:53 +0100

    version bump

--- src/modules/env.js
+++ src/modules/env.js
@@ -5,7 +5,7 @@ export const env = {
    first_time_user: false,
 
    // version is Major.minor.added.changed.fixed
-   version: '1.5.1.5.4.alpha',
+   version: '1.5.3.13.9',
    version_check: undefined,
    reset_new_versions: false,
 
